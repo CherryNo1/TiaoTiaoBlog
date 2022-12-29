7c3b0823d42c874ae84392685d484dd2bed090a4
马青波 <qingbo.ma@hand-china.com>
2022-12-29 09:23:14 +0800

    原生js base64加密解密

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -32,11 +32,15 @@ import style from "./index.module.scss";
 import SiderComp from "./components/SiderComp";
 // const phone = decode("bWFxaTUxNDc2OG5nYm8xNTU0MQ==");
 
-//原生js
-// decodeURIComponent(
-//   escape(window.atob("5oiR5piv5LiA5q616ZyA6KaB5aSE55CG55qE5a2X56ym"))
-// );
-
+import base64 from "js-base64";
+const phone = base64.decode("7784854978686949778481517810610361");
+//原生js加密
+window.btoa(unescape(encodeURIComponent("我是一段需要处理的字符")));
+//原生js解密
+decodeURIComponent(
+  escape(window.atob("5oiR5piv5LiA5q616ZyA6KaB5aSE55CG55qE5a2X56ym"))
+);
+console.log(phone);
 
 const Home: React.FC = (props) => {
   const {
